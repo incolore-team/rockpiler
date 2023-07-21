af550f447f2d22e27a8256196fb68f1842511cab
pluveto <i@pluvet.com>
2023-07-21 11:58:51 +0800

    chore: relayout project

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,6 @@
+extern crate lazy_static;
 #[macro_use]
 extern crate pest_derive;
-extern crate lazy_static;
-
 pub const VERSION: &str = env!("CARGO_PKG_VERSION");
 pub mod ast;
 pub mod cli;
@@ -9,10 +8,13 @@ pub mod driver;
 pub mod infer_eval;
 pub mod ir;
 pub mod ir_builder;
+pub mod ir_pass;
 pub mod ir_printer;
+pub mod mc;
+pub mod mc_builder;
+pub mod mc_printer;
 pub mod parser;
 pub mod scope;
 pub mod sema;
 pub mod symbol;
-pub mod pass;
 pub mod util;
